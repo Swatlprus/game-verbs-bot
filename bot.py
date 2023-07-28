0737bf62c79514d56f9f3b863ea3b3e543a07277
Rinat <swatlprus@gmail.com>
2023-07-28 10:31:50 +0500

    Setup DialogFlow

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
 from environs import Env
+import functools
+from google.cloud import dialogflow
 import logging
 from telegram import Update, ForceReply
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
@@ -8,6 +10,22 @@ logging.basicConfig(
 )
 
 logger = logging.getLogger(__name__)
+
+
+def detect_intent_texts(project_id, session_id, texts, language_code='ru'):
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+
+    for text in texts:
+        text_input = dialogflow.TextInput(text=text, language_code=language_code)
+
+        query_input = dialogflow.QueryInput(text=text_input)
+
+        response = session_client.detect_intent(
+            request={"session": session, "query_input": query_input}
+        )
+        check = response.query_result.fulfillment_text
+        return check
 
 
 def start(update: Update, context: CallbackContext) -> None:
@@ -24,32 +42,23 @@ def help_command(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Help!')
 
 
-def echo(update: Update, context: CallbackContext) -> None:
+def echo(project_id, session_id, update: Update, context: CallbackContext) -> None:
     """Echo the user message."""
-    update.message.reply_text(update.message.text)
+    check = detect_intent_texts(project_id, session_id, [update.message.text], 'ru')
+    update.message.reply_text(check)
 
 
-def main(telegram_token) -> None:
-    """Start the bot."""
-    # Create the Updater and pass it your bot's token.
+def main(telegram_token, project_id, session_id) -> None:
     updater = Updater(telegram_token)
-
-    # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
 
-    # on different commands - answer in Telegram
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help_command))
 
-    # on non command i.e message - echo the message on Telegram
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    end = functools.partial(echo, project_id, session_id)
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, end))
 
-    # Start the Bot
     updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
 
 
@@ -57,4 +66,6 @@ if __name__ == '__main__':
     env = Env()
     env.read_env()
     telegram_token = env('TELEGRAM_TOKEN')
-    main(telegram_token)
+    project_id = env('PROJECT_ID')
+    session_id = env('SESSION_ID')
+    main(telegram_token, project_id, session_id)
